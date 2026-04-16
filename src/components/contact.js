8835ea4aa39dc5fecac7ce3716efde5f5dd4be70
Olya Rykova <oli.andrv96@gmail.com>
2026-04-16 17:08:29 +0200

    add block contact

---
+++ src/components/contact.js
@@ -0,0 +1,21 @@
+export function Contact() {
+  return `
+<section class="bg-black">
+    <div class="flex h-17.5">
+        <img src="./src/image/contact/bg-w.png" alt="bg-w">
+    </div>
+  <div class="container max-w-7xl mx-auto h-full px-0">
+    <div class="bg-black pt-1.5 px-3 pb-17.5 md:pt-3.75 md:px-9 lg:px-0">
+        <div class="flex flex-col">
+            <img src="./src/image/contact/promotion.svg" alt="promotion" class="w-15.5 h-15.5">
+            <img src="./src/image/contact/console-m.png" alt="console" class="m-h-[131px] -mt-3.25 md:hidden">
+        </div>
+        <div class="flex flex-col gap-6.25">
+        <h2 class="text-white text-[30px] leading-7.5 lg:text-[62px] lg:leading-15.5">Играй и выигрывай!</h2>
+        <p class="text-white">Играй в <span class="text-blue">Cyberpunk 2077</span> и получи возможность выиграть консоль <span class="text-blue">Xbox Series X</span> или <sapn class="text-blue">Sony PlayStation 5!</sapn> Заполни форму ниже и приложи скриншот о покупке игры. Итоги розыгрыша будут подведены 1 февраля. Удачи! ;)</p>
+        </div>
+    </div>
+   </div>
+</section>
+  `;
+}
--- src/main.js
+++ src/main.js
@@ -4,17 +4,19 @@ import { Header } from "./components/header.js";
 import { Hero } from "./components/hero.js";
 import { Footer } from "./components/footer.js";
 import { About } from "./components/about.js";
+import { Contact } from "./components/contact.js";
 
 const app = document.querySelector("#app");
 
 app.innerHTML = `
   <div class="min-h-screen flex flex-col">
 
-    <div class="header-hero bg-cover bg-center flex-grow">
+    <div class="header-hero bg-cover bg-center grow">
       ${Header()}
       ${Hero()}
     </div>
-      ${About()}
+    ${About()}
+    ${Contact()}
 
     ${Footer()}
   </div>
