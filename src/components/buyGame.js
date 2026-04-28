b4078e8581c6096fdd7e9b0c7a945ee956bf5913
Olya Rykova <oli.andrv96@gmail.com>
2026-04-28 21:27:00 +0200

    fix buyGame

--- src/components/buyGame.js
+++ src/components/buyGame.js
@@ -1,55 +1,58 @@
 export function Game() {
   return `
 <section class="bg-black">
-  <div class="flex flex-col">
+
+  <div class="flex flex-col lg:flex-row">
     
-    <div class=" lg:order-none">
+    <div class="w-full h-50 md:h-62.5 lg:w-83 lg:h-auto overflow-hidden xl:w-[45%]">
       <img
-        src="./src/image/game/bg-full.jpg" alt="Cyberpunk editions" class="w-full max-h-50 object-cover"
+        src="./src/image/game/bg-full.jpg" alt="Cyberpunk editions" class="w-full h-full object-cover"
       />
     </div>
-
-    <div class=" px-3 pt-10 pb-17.5 sm:px-8 md:px-12 lg:px-16 xl:px-20 flex flex-col justify-center">
-      <h2 class="text-white text-[30px] lg:text-[62px] leading-tight mb-6.25">
-        Купить игру <br />
-        Cyberpunk 2077
+    <div class="px-3 pt-10 pb-17.5 lg:pt-17.5 lg:px-8 flex flex-col justify-center">
+      <h2 class="text-white text-[30px] lg:text-[62px] leading-tight mb-6.25 max-w-156.25">
+        Купить игру Cyberpunk 2077
       </h2>
 
-      <div class="mb-10">
-        <h3 class="text-cyan-400 text-xl font-bold mb-5">
+      <div class="mb-10 lg:mb-17.5">
+        <h3 class="text-blue text-[20px] font-bold mb-5 lg:mb-3.75 lg:text-[24px]">
           В комплект входит:
         </h3>
 
-        <ul class="space-y-4 text-white text-base sm:text-lg">
-          <li class="flex items-center gap-3">
-            <span class="w-5 h-5 rounded-full border-2 border-yellow-400 flex items-center justify-center">
-              <span class="w-2 h-2 rounded-full bg-yellow-400"></span>
-            </span>
-            Футляр с игровыми дисками
+        <ul class="flex flex-col gap-3.75 text-white text-base sm:text-lg">
+          <li class="flex items-start gap-4.5 lg:gap-2.5 lg:items-center">
+              <img src="./src/image/game/img1.svg" alt="icon" class="w-5 h-5 lg:w-8 lg:h-8 object-cover mt-1.25 lg:mt-0"/>
+            <p class="font-roboto text-[16px] lg:text-[18px]">Футляр с игровыми дисками</p>
           </li>
 
-          <li class="flex items-center gap-3">
-            <span class="w-5 h-5 border-2 border-yellow-400"></span>
-            Футляр с кодом для загрузки игры и дисками (PC)
+          <li class="flex items-start gap-4.5 lg:gap-2.5 lg:items-center">
+            <img src="./src/image/game/img2.svg" alt="icon" class="w-5 h-5 lg:w-8 lg:h-8 object-cover mt-1.25 lg:mt-0"/>
+
+            <p class="font-roboto text-[16px] lg:text-[18px]">Футляр с кодом для загрузки игры и дисками (PC)</p>
           </li>
 
-          <li class="flex items-center gap-3">
-            <span class="w-5 h-5 border-2 border-yellow-400"></span>
-            Справочник с информацией об игровом мире
+          <li class="flex items-start gap-4.5 lg:gap-2.5 lg:items-center">
+            <img src="./src/image/game/img3.svg" alt="icon" class="w-5 h-5 lg:w-8 lg:h-8 object-cover mt-1.25 lg:mt-0"/>
+
+            <p class="font-roboto text-[16px] lg:text-[18px]">Справочник с информацией об игровом мире</p>
           </li>
         </ul>
       </div>
 
       <div>
-        <h3 class="text-cyan-400 text-xl font-bold mb-6">
+        <h3 class="text-blue text-[20px] font-bold mb-5 lg:mb-3.75 lg:text-[24px]">
           Выберите платформу:
         </h3>
 
-        <div class="grid grid-cols-2 gap-6 items-center">
-          <img src="/icons/pc.svg" alt="PC" class="h-10 object-contain" />
-          <img src="/icons/xbox.svg" alt="Xbox" class="h-10 object-contain" />
-          <img src="/icons/stadia.svg" alt="Stadia" class="h-10 object-contain" />
-          <img src="/icons/ps4.svg" alt="PS4" class="h-10 object-contain" />
+        <div class="flex flex-col gap-10 lg:gap-11.25">
+        <div class="flex gap-14 lg:gap-20">
+          <img src="./src/image/game/pc.svg" alt="PC" class="w-auto object-contain" />
+          <img src="./src/image/game/xboxone.svg" alt="Xbox" class="w-auto object-contain" />
+        </div>
+        <div class="flex gap-14 lg:gap-20">
+          <img src="./src/image/game/stadia.svg" alt="Stadia" class="w-auto object-contain" />
+          <img src="./src/image/game/playstation.svg" alt="PS4" class="w-auto object-contain" />
+          </div>
         </div>
       </div>
     </div>
